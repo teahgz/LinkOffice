20afb346a58fe54e15c6d9e20ca7900dba85c6ba
Chaeyoung <chaedud02@naver.com>
2024-08-29 15:46:13 +0900

    [master] 브랜치 생성

---
+++ src/main/java/com/fiveLink/linkOffice/LinkOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.fiveLink.linkOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiveLink/linkOffice/LinkOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiveLink.linkOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
