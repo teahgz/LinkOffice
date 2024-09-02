a3354f27aa25372f7c025f4bc8c5fd5e050228ff
teahgz <teahgz@gmail.com>
2024-09-02 15:30:21 +0900

    :sparkles: [전주영] homeController memberDto 생성재업로드

--- src/main/java/com/fiveLink/linkOffice/member/repository/MemberRepository.java
+++ src/main/java/com/fiveLink/linkOffice/member/repository/MemberRepository.java
@@ -11,11 +11,8 @@ import com.fiveLink.linkOffice.member.domain.Member;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     
-<<<<<<< HEAD
-    Member findByMemberNumber(String memberNumber);
-    
-    
-=======
+
+
     Member findByMemberNumber(String memberNumber); 
     
     // [서혜원] 조직도
@@ -24,5 +21,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("SELECT m FROM Member m WHERE m.positionNo = :positionNo")
     List<Member> findByPositionNo(Long positionNo);
->>>>>>> d3c793ac092aa64df6bb90d0381f5e37ae029679
+
 }
