75016090bd60eccbb228ad0591a614a82c25d999
teahgz <teahgz@gmail.com>
2024-09-02 23:16:25 +0900

    :sparkles: [전주영] mypage 정보 조회

--- src/main/java/com/fiveLink/linkOffice/member/controller/MemberViewController.java
+++ src/main/java/com/fiveLink/linkOffice/member/controller/MemberViewController.java
@@ -1,14 +1,32 @@
 package com.fiveLink.linkOffice.member.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fiveLink.linkOffice.member.domain.MemberDto;
+import com.fiveLink.linkOffice.member.service.MemberService;
 
 @Controller
 public class MemberViewController {
 	
-	@GetMapping("/employee/member/mypage")
-	public String myPage() {
-		return "employee/member/mypage";
+	private final MemberService memberService;
+	
+	@Autowired
+	public MemberViewController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
+	@GetMapping("/employee/member/mypage/{member_no}")
+	public String myPage(@PathVariable("member_no") Long memberNo, Model model) {
+		List<MemberDto> memberdto = memberService.getMembersByNo(memberNo); 
+	    model.addAttribute("memberdto", memberdto);
+	    System.out.println(memberdto);
+	    return "employee/member/mypage";
 	}
 	
 	@GetMapping("/employee/member/myedit")
--- src/main/java/com/fiveLink/linkOffice/member/repository/MemberRepository.java
+++ src/main/java/com/fiveLink/linkOffice/member/repository/MemberRepository.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fiveLink.linkOffice.member.domain.Member;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    
-
 
     Member findByMemberNumber(String memberNumber); 
+    
+    // mypage 정보 조회
+    @Query("SELECT m, p.positionName, d.departmentName " +
+            "FROM Member m " +
+            "JOIN m.position p " +
+            "JOIN m.department d " +
+            "WHERE m.memberNo = :memberNo")
+     List<Object[]> findMemberWithDepartmentAndPosition(@Param("memberNo") Long memberNo); 
+
     
     // [서혜원] 조직도
     @Query("SELECT m FROM Member m WHERE m.departmentNo = :departmentNo")
