b91dc435cea0eb17e87da997cdad5870adb42910
HyeSun <hsp0309@naver.com>
2024-09-04 13:03:10 +0900

    :sparkles: [박혜선] 퇴근 기능 구현 및 버튼 disabled 수정

--- src/main/resources/static/js/employee/attendance/attendance.js
+++ src/main/resources/static/js/employee/attendance/attendance.js
@@ -30,91 +30,95 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     // memberNo 값 가져오기 
     var memberNo = document.getElementById('attendance_memberNo').value;
+	
+	// 출퇴근 버튼 상태 
+	var checkInButton = document.getElementById('check_in_button');
+    var checkOutButton = document.getElementById('check_out_button');
+	
+    // 출근버튼이 활성화된 상태라면 출근 기능 실행 
+    if(!checkInButton.disabled){		
+	    document.getElementById('check_in_button').addEventListener('click', function() {
+			// submit이 자동으로 되지 않게 해줌 
+			event.preventDefault();
+			
+            var csrfToken = document.querySelector('input[name="_csrf"]').value;
+            var url = '/attendance/checkIn';
+            var jsonData = JSON.stringify({ memberNo: memberNo });
 
-    // 출근 기능 
-    document.getElementById('check_in_button').addEventListener('click', function() {
-		// submit이 자동으로 되지 않게 해줌 
-		event.preventDefault();
-		
-        var csrfToken = document.querySelector('input[name="_csrf"]').value;
-        var url = '/attendance/checkIn';
-        var jsonData = JSON.stringify({ memberNo: memberNo });
+            // 버튼 비활성화 및 스타일 업데이트
+            checkInButton.disabled = true;
 
-        // 버튼 비활성화 및 스타일 업데이트
-        var button = document.getElementById('check_in_button');
-        button.disabled = true;
-        button.style.backgroundColor = '#eee';
-
-        // AJAX 요청 보내기
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Requested-With': 'XMLHttpRequest',
-                'X-CSRF-TOKEN': csrfToken
-            },
-            body: jsonData
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.res_code === '200') {
-                Swal.fire({
-                    icon: 'success',
-                    title: '출석 확인',
-                    text: data.res_msg,
-                    confirmButtonText: '확인'
-                });
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: '오류',
-                    text: data.res_msg,
-                    confirmButtonText: '확인'
-                });
-            }
-        });
-    });
+            // AJAX 요청 보내기
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-CSRF-TOKEN': csrfToken
+                },
+                body: jsonData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.res_code === '200') {
+                    Swal.fire({
+                        icon: 'success',
+                        title: '출석 확인',
+                        text: data.res_msg,
+                        confirmButtonText: '확인'
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: '오류',
+                        text: data.res_msg,
+                        confirmButtonText: '확인'
+                    });
+                }
+            });
+	    });
+	}
    	// 퇴근 기능 
-    document.getElementById('check_out_button').addEventListener('click', function() {
-		// submit이 자동으로 되지 않게 해줌 
-		event.preventDefault();
-		
-        var csrfToken = document.querySelector('input[name="_csrf"]').value;
-        var url = '/attendance/checkOut';
-        var jsonData = JSON.stringify({ memberNo: memberNo });
+    if(!checkOutButton.disabled){		
+	    document.getElementById('check_out_button').addEventListener('click', function() {
+			// submit이 자동으로 되지 않게 해줌 
+			event.preventDefault();
+			
+            var csrfToken = document.querySelector('input[name="_csrf"]').value;
+            var url = '/attendance/checkOut';
+            var jsonData = JSON.stringify({ memberNo: memberNo });
 
-        // 버튼 비활성화 및 스타일 업데이트
-        var button = document.getElementById('check_out_button');
-        button.disabled = true;
-        button.style.backgroundColor = '#eee';
+            // 버튼 비활성화 및 스타일 업데이트
+            checkOutButton.disabled = true;
 
-        // AJAX 요청 보내기
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Requested-With': 'XMLHttpRequest',
-                'X-CSRF-TOKEN': csrfToken
-            },
-            body: jsonData
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.res_code === '200') {
-                Swal.fire({
-                    icon: 'success',
-                    title: '퇴근 확인',
-                    text: data.res_msg,
-                    confirmButtonText: '확인'
-                });
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: '오류',
-                    text: data.res_msg,
-                    confirmButtonText: '확인'
-                });
-            }
-        });
-    });
+            // AJAX 요청 보내기
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-CSRF-TOKEN': csrfToken
+                },
+                body: jsonData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.res_code === '200') {
+                    Swal.fire({
+                        icon: 'success',
+                        title: '퇴근 확인',
+                        text: data.res_msg,
+                        confirmButtonText: '확인'
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: '오류',
+                        text: data.res_msg,
+                        confirmButtonText: '확인'
+                    });
+                }
+            });
+	    });
+	}
 });
