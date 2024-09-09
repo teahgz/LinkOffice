447b32808460bf2275e3f9b0d44a043cce2a1fbb
Kim-Minjae1 <rushnkash@gmail.com>
2024-09-09 14:05:59 +0900

    :sparkles: [김민재] 비품관리 css 수정

--- src/main/java/com/fiveLink/linkOffice/vacation/controller/VacationViewController.java
+++ src/main/java/com/fiveLink/linkOffice/vacation/controller/VacationViewController.java
@@ -1,12 +1,7 @@
 package com.fiveLink.linkOffice.vacation.controller;
 
-import com.fiveLink.linkOffice.member.domain.Member;
-import com.fiveLink.linkOffice.member.domain.MemberDto;
-import com.fiveLink.linkOffice.member.service.MemberService;
-import com.fiveLink.linkOffice.vacation.domain.VacationDto;
-import com.fiveLink.linkOffice.vacation.domain.VacationStandardDto;
-import com.fiveLink.linkOffice.vacation.domain.VacationTypeDto;
-import com.fiveLink.linkOffice.vacation.service.VacationService;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import com.fiveLink.linkOffice.member.domain.MemberDto;
+import com.fiveLink.linkOffice.member.service.MemberService;
+import com.fiveLink.linkOffice.vacation.domain.VacationDto;
+import com.fiveLink.linkOffice.vacation.domain.VacationStandardDto;
+import com.fiveLink.linkOffice.vacation.domain.VacationTypeDto;
+import com.fiveLink.linkOffice.vacation.service.VacationService;
 
 @Controller
 public class VacationViewController {
@@ -65,7 +64,7 @@ public class VacationViewController {
         List<MemberDto> memberdto = memberService.getMembersByNo(memberNo);
         int countStandard = vacationService.countStandard();
 
-        List<VacationStandardDto> vacationStandard = vacationService.selectVacationStandard();
+        List<VacationStandardDto> vacationStandard = vacationService.selectVacatiSonStandard();
         model.addAttribute("memberdto", memberdto);
         model.addAttribute("countStandard", countStandard);
         model.addAttribute("vacationStandard", vacationStandard);
