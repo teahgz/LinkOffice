28097807cf15cb0fffda5dd0a8446b2e1e62275b
teahgz <teahgz@gmail.com>
2024-09-11 00:05:04 +0900

    :sparkles: [전주영] 전자결재 양식 수정 완료

--- src/main/java/com/fiveLink/linkOffice/approval/controller/ApprovalApiController.java
+++ src/main/java/com/fiveLink/linkOffice/approval/controller/ApprovalApiController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -33,14 +34,31 @@ public class ApprovalApiController {
 		 dto.setApproval_form_title(approvalTitle);  
 		 dto.setApproval_form_content(editorContent);
 		 dto.setApproval_form_status(0L);
-		 System.out.println("controller"+dto);
 		    
 		 if(approvalFormService.saveApprovalForm(dto) != null) {
 				response.put("res_code", "200");
 			    response.put("res_msg", "양식 등록을 성공하였습니다.");			 
 		 }
-		
 	    return response; 
-
+	}
+	
+	// 관리자 전자결재 양식 수정
+	@ResponseBody
+	@PutMapping("/admin/approval/edit")
+	public Map<String,String> adminApprovalEdit(@RequestParam("form_no") String formNo,@RequestParam("approval_title") String approvalTitle, @RequestParam("editor_content") String editorContent) {
+		Map<String, String> response = new HashMap<>();
+	    response.put("res_code", "404");
+	    response.put("res_msg", "양식 수정 중 오류가 발생하였습니다.");
+		 ApprovalFormDto dto = new ApprovalFormDto();
+		 dto.setApproval_form_no(Long.parseLong(formNo));  
+		 dto.setApproval_form_title(approvalTitle);  
+		 dto.setApproval_form_content(editorContent);
+		 dto.setApproval_form_status(0L);
+		    
+		 if(approvalFormService.editApprovalForm(dto) != null) {
+				response.put("res_code", "200");
+			    response.put("res_msg", "양식 수정을 성공하였습니다.");			 
+		 }
+	    return response; 
 	}
 }
