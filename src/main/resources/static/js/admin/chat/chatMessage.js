ecb85eda33d949d307eccca2af802a24aa1657c9
Chaeyoung <chaedud02@naver.com>
2024-09-19 10:58:02 +0900

    :sparkles: [김채영] 그룹 채팅방 이름 css와 모달 js 수정

--- src/main/resources/static/js/admin/chat/chatMessage.js
+++ src/main/resources/static/js/admin/chat/chatMessage.js
@@ -46,6 +46,42 @@ document.addEventListener("DOMContentLoaded", function() {
            searchMem();
         }
     };
+       function closeEditChat() {
+           const editChatRoomModal = document.getElementById('editChatRoomModal');
+          $('#editChatRoomModal').modal('hide');
+       }
+       const closeButton = document.querySelector('.modal-content-element button');
+       if (closeButton) {
+           closeButton.addEventListener('click', closeEditChat);
+       } else {
+           console.error("닫기 버튼을 찾을 수 없습니다.");
+       }
+
+
+    const editChatRoomNameInput = document.getElementById('chatRoomNameInput');
+    const confirmEditButton = document.getElementById('confirmEditButton');
+
+    function toggleConfirmEditButton() {
+        confirmEditButton.disabled = editChatRoomNameInput.value.trim() === '';
+         if (confirmEditButton.disabled) {
+                confirmEditButton.classList.add('button-disabled');
+            } else {
+                confirmEditButton.classList.remove('button-disabled');
+            }
+    }
+
+    editChatRoomNameInput.addEventListener('input', toggleConfirmEditButton);
+    toggleConfirmEditButton();
+
+        $('#editChatRoomModal').on('show.bs.modal', function () {
+            toggleConfirmEditButton();
+        });
+
+      $('#editChatRoomModal').on('hide.bs.modal', function () {
+          editChatRoomNameInput.value = '';
+          confirmEditButton.disabled = true;
+          confirmEditButton.classList.add('update-button');
+      });
 });
 
 (function() {
@@ -203,7 +239,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 resetSelectedMembers();
                 $('#organizationChartModal').modal('hide');
             }
-        }else if(message.type === "chatRoomUpdate"){
+        } else if(message.type === "chatRoomUpdate"){
             const updatedChatRoomNo = message.roomNo;
             const updatedChatRoomName = message.updatedChatRoomName;
             const chatItem = document.querySelector(`input[value="${updatedChatRoomNo}"]`);
@@ -218,6 +254,7 @@ document.addEventListener("DOMContentLoaded", function() {
                const chatRoomTitleElement = document.getElementById("chatRoomTitle");
                chatRoomTitleElement.textContent = updatedChatRoomName;
            }
+           document.getElementById('chatRoomNameInput').value = '';
         } else {
             const messageElement = document.createElement("div");
             const now = new Date();
@@ -324,8 +361,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById('editChatRoomButton').addEventListener('click', function(event) {
             event.preventDefault();
+
             $('#editChatRoomModal').modal('show');
         });
+
 
 
     document.getElementById('openDrop').addEventListener('click', function(event) {
