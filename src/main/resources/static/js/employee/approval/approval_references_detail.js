fb615a4b966c23b196709e09baad7ed11b8e4189
teahgz <teahgz@gmail.com>
2024-09-29 01:54:08 +0900

    :sparkles: [전주영] 해당 목록 진입 시 알림 제거 수정

--- src/main/resources/static/js/employee/approval/approval_references_detail.js
+++ src/main/resources/static/js/employee/approval/approval_references_detail.js
@@ -227,37 +227,4 @@ document.addEventListener('DOMContentLoaded', function() {
 			};
 
 		});
-		
-	window.functionType = 4;
-    console.log("현재 기능 타입: " +window.functionType);
-
-    if (window.functionType === 4) {
-        markNotificationsAsRead(window.functionType);
-    } 
-    
-    if (currentType === window.functionType) {
-                    markNotificationsAsRead(window.functionType);
-                } else {
-                    if (notificationModal.children.length <= 1) {
-                        notificationModal.innerHTML = `
-                           <li id="mark-as-read" class="mark-as-read" style="font-size: 10px; text-align: right; color: gray;">일괄읽음</li>
-                        `;
-                        addMarkAsReadListener();
-                    }
-                    message.data.forEach(function(item) {
-                        showNotification(title, content, item.memberNo, message.timestamp);
-                        const listItem = document.createElement('li');
-
-                        listItem.setAttribute('data-notification-no', item.nofication_pk + 1);
-                        listItem.innerHTML = `
-                            <strong style="margin-bottom: 5px;">${title}</strong>
-                            <p>${content}</p>
-                            <em style="display: block; margin-bottom: 5px; float: right;">${message.timestamp}</em>
-                            <hr style="border: none; margin: 10px 0;">
-                        `;
-
-                        notificationModal.insertBefore(listItem, notificationModal.children[1]);
-                    });
-                }
-
 });
